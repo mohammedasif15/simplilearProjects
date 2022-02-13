ca89a904200e7c71ac28c267442cebc7efb8682a
mohammedasif15 <96732110+mohammedasif15@users.noreply.github.com>
2022-02-13 19:00:29 +0530

    Add files via upload

---
+++ Assisted341/src/main/java/com/ecommerce/Assisted341Application.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assisted341Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assisted341Application.class, args);
+	}
+
+}
