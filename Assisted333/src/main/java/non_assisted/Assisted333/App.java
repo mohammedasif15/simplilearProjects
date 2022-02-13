ca0d8ae0f098bc4fb9c42f251f9faf1dd5ef680e
mohammedasif15 <96732110+mohammedasif15@users.noreply.github.com>
2022-02-13 18:58:34 +0530

    Add files via upload

---
+++ Assisted333/src/main/java/non_assisted/Assisted333/App.java
@@ -0,0 +1,13 @@
+package non_assisted.Assisted333;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
