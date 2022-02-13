c8eafbc8f138f312f280d70fad2e6ccb944ccafa
mohammedasif15 <96732110+mohammedasif15@users.noreply.github.com>
2022-02-13 18:59:59 +0530

    Add files via upload

---
+++ Assisted336/src/test/java/non_assisted/Assisted336/AppTest.java
@@ -0,0 +1,20 @@
+package non_assisted.Assisted336;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
