e77ccc629f2a57849061a997efbfd9205510d73a
mohammedasif15 <96732110+mohammedasif15@users.noreply.github.com>
2022-02-13 18:55:34 +0530

    Add files via upload

---
+++ Assisted322/src/main/java/com/ecommerce/Assisted322Application.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assisted322Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assisted322Application.class, args);
+	}
+
+}
---
+++ Assisted322/src/test/java/com/ecommerce/Assisted322ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assisted322ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
